e13c8e3dc4a7ca0358ccbf82f830dd8b5d38bfdb
janus6685@gamil.com <janus6685@gamil.com>
2017-08-08 00:15:56 +0900

    unreal Project push

---
+++ trunk/Project_F/Source/Project_F/Project_FGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project_FGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_F_API AProject_FGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
